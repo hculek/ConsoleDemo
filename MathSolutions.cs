34845c3f75834f3c0e503d091db5aac3d5a81096
Hrvoje Culek <hhcc>
2022-03-16 21:55:54 +0100

    Added class and method to sum even numbers in array.

---
+++ MathSolutions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hculekConsoleDemoModules
+{
+    public static class MathSolutions
+    {
+        public static void ArraySumEvenNumbers() 
+        {
+
+            int a = 50;
+            int[] numbers = new int[a];
+
+            for (int i = 1; i < a+1; i++)
+            {
+                numbers[i - 1] = i;
+            }
+            var result = numbers.Where(i => i % 2 == 0).Sum(i=> (long)i);
+
+
+            Console.WriteLine("Sum of even numbers in array is {0}", result);
+
+        }
+
+    }
+}
